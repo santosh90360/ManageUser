2bd7c8281686040b41223b17d13f1f03b6073b07
Santosh Singh <santosh3033@gmail.com>
2023-02-13 14:42:07 +0530

    Read row-data json file

---
+++ ManageUser/Controllers/ManageGridController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ManageUser.Models;
+using Newtonsoft.Json;
+using System.Net;
+using Microsoft.DotNet.MSIdentity.Shared;
+
+namespace ManageUser.Controllers
+{
+    public class ManageGridController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Fetch the JSON string from URL.
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            string json = (new WebClient()).DownloadString("https://ag-grid.com/example-assets/row-data.json");
+            var products = JsonConvert.DeserializeObject<IEnumerable<Product>>(json); //Deserialize Object     
+            return View(products);
+        }
+    }
+}
---
+++ ManageUser/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace ManageUser.Models
+{
+	public class Product
+	{
+		public string make { get; set; }
+		public string model { get; set; }
+		public int price { get; set; }	
+	}
+}
